35b231411042845c9099ac90b2266775bb1ab420
SheilaKhe <s.khedoo@outlook.fr>
2025-08-28 19:17:46 +0200

    Add modifications2 to fix deployment

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, os
 
 app = Flask(__name__)
 
@@ -249,4 +249,5 @@ def get_prices():
         return jsonify({"error": str(e)}), 500
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=3000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
